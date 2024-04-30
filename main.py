5a1784b9c394702e6b626cbf77d135c2f3f8204e
ryancbell <165860690+ryancbell@users.noreply.github.com>
2024-04-30 14:35:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # Arnav Code Space Project 1
-name = input("Enter your name ")
+name = input("Enter your name: ")
 print("Hello " + name )
 
 userInput = input("Press Enter to continue")
+
 print("\n**********************************************************************")
 
 # Promt for users interests
@@ -43,18 +44,26 @@ print("\n")
 userInput = input("Press Enter to continue")
 print("\n**********************************************************************")
 
-if interest == "1":
-    print("Congrats! You have choosen the Computing and IT pathway!")
-    print("You should take the classes STEM 101, STEM 103, and English 101!")
-elif interest == "2":
-    print("Congrats! You have choosen the Science pathway!")
-    print("You should take the classes STEM 101, STEM 102, and Pre Calculus!")
-elif interest == "2":
-    print("Congrats! You have choosen the Engineering pathway!")
-    print("You should take the classes STEM 101, ENGR 111, and Pre Calculus!")
-elif interest == "2":
-    print("Congrats! You have choosen the Health Sciences pathway!")
-    print("You should take the classes STEM 101, STEM 102, and English 101!")
+while True:
+    if interest == "1":
+        print("Congrats! You have choosen the Computing and IT pathway!")
+        print("You should take the classes STEM 101, STEM 103, and English 101!")
+        break
+    elif interest == "2":
+        print("Congrats! You have choosen the Science pathway!")
+        print("You should take the classes STEM 101, STEM 102, and Pre Calculus!")
+        break
+    elif interest == "3":
+        print("Congrats! You have choosen the Engineering pathway!")
+        print("You should take the classes STEM 101, ENGR 111, and Pre Calculus!")
+        break
+    elif interest == "4":
+        print("Congrats! You have choosen the Health Sciences pathway!")
+        print("You should take the classes STEM 101, STEM 102, and English 101!")
+        break
+    else:
+        print("Incorrect direction, please try again.")
+        interest = input("")
 
 print("\n")
 userInput = input("Press Enter to continue")

d2b9497c8983e1ec775ded6382f871212c575b93
romelpalayon07 <165983784+romelpalayon07@users.noreply.github.com>
2024-04-30 20:20:07 +0000

    Updated

--- romel.py
+++ romel.py
@@ -1,16 +1,22 @@
-input("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n  This is a personality test. \n Please choose an option.\n\n~~~~Press Enter to Continue~~~~~\n")
+#input("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n  This is a personality test. \n Please choose an option.\n\n~~~~Press Enter to Continue~~~~~\n")
 
-ChoiceOne = input("You're walking through the hallway and you see that someone has spilled their coffee:\na) You help them clean it up\nb) You ignore them because you think someone else will help them\n")
+#ChoiceOne = input("You're walking through the hallway and you see that someone has spilled their coffee:\na) You help them clean it up\nb) You ignore them because you think someone else will help them\n")
 
-if ChoiceOne == "a":
-    input("You come up to them and offer your help. They appreciate it and pays you back with a free cup of coffee :]\n\n~~~~Press Enter to Continue~~~~~\n")
+#if ChoiceOne == "a":
+    #input("You come up to them and offer your help. They appreciate it and pays you back with a free cup of coffee :]\n\n~~~~Press Enter to Continue~~~~~\n")
 
-elif ChoiceOne == "b":
-    input("You comepletely walk by them and try to ignore their struggle (They will remember that)\n\n~~~~Press Enter to Continue~~~~~\n")
+#elif ChoiceOne == "b":
+    #input("You comepletely walk by them and try to ignore their struggle (They will remember that)\n\n~~~~Press Enter to Continue~~~~~\n")
 
-else:
-    print("That is not an option")
-ChoiceTwo = input("\nPlease choose for your second options:\na) choice a\nb) choice b\n")
-ChoiceThree = input("\nPlease choose for your third option:\na) choice a\nb) choice b\n")
+#else:
+    #print("That is not an option")
+#ChoiceTwo = input("\nPlease choose for your second options:\na) choice a\nb) choice b\n")
+#ChoiceThree = input("\nPlease choose for your third option:\na) choice a\nb) choice b\n")
+print("\n**********************************************************************")
+print("You start on your adventure, and you stumble across a four-forked road.")
+print("Each road has a sign that is associated with a program.\nPlease enter your area of interest (Input 1, 2, 3, or 4)\n")
+
+interest = input("1 - Computing and IT\n2 - Science\n3 - Engineering\n4 - Health Sciences\n")
+
 
 
